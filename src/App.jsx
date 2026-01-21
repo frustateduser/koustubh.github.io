452fb0feb678b0f962bb16f9f82c12ad69504c19
frustateduser <koustubhbadshah07@outlook.com>
2026-01-21 20:34:47 +0530

    feat: implement GitHub Actions workflow for Vite React app deployment and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import "./index.css";
 import MainBody from "./pages/MainBody";
 import Aboutme from "./pages/Aboutme";
 import Contactme from "./pages/Contactme";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainBody />} />
         <Route path="/about" element={<Aboutme />} />
         <Route path="/contact" element={<Contactme />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
